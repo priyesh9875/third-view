b059487ef2c1107d41a4c5f6ab0eb1a989615942
priyesh9875 <priyesh9875@gmail.com>
2016-06-14 03:31:59 +0530

    clean up!

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/bootstrap/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/sprintf-js/index.d.ts" />
-/// <reference path="globals/stats/index.d.ts" />
-/// <reference path="globals/three/index.d.ts" />
